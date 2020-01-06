0df6a14395ea5d2cd45c8eb715f8069e79adad03
taramouse <nickykeyse@gmail.com>
2020-01-06 21:30:15 +0000

    Added alert type and show booleen props.

--- components/NKNotification/NKNotification.stories.js
+++ components/NKNotification/NKNotification.stories.js
@@ -2,14 +2,63 @@ import { storiesOf } from '@storybook/vue'
 import NKNotification from './NKNotification'
 
 storiesOf('NKNotification', module)
-  .add('normal', () => ({
+  .add('default', () => ({
     components: { NKNotification },
     template: `
       <NKNotification
         :msg="msg"
+        :alertType="alertType"
+        :show="show"
       />
     `,
     data: () => ({
-      msg: "This is a notification."
+      msg: "This is an default notification.",
+      alertType: "nk-default",
+      show: true
+    })
+  }))
+  .add('success', () => ({
+    components: { NKNotification },
+    template: `
+      <NKNotification
+        :msg="msg"
+        :alertType="alertType"
+        :show="show"
+      />
+    `,
+    data: () => ({
+      msg: "This is an success notification.",
+      alertType: "nk-success",
+      show: true
+    })
+  }))
+  .add('warning', () => ({
+    components: { NKNotification },
+    template: `
+      <NKNotification
+        :msg="msg"
+        :alertType="alertType"
+        :show="show"
+      />
+    `,
+    data: () => ({
+      msg: "This is an warning notification.",
+      alertType: "nk-warning",
+      show: true
+    })
+  }))
+  .add('error', () => ({
+    components: { NKNotification },
+    template: `
+      <NKNotification
+        :msg="msg"
+        :alertType="alertType"
+        :show="show"
+      />
+    `,
+    data: () => ({
+      msg: "This is an error notification.",
+      alertType: "nk-error",
+      show: true
     })
   }))
--- components/NKNotification/NKNotification.vue
+++ components/NKNotification/NKNotification.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="alert">
+  <div v-if="show"
+       class="nk-alert"
+       :class="computedClass"
+       :alertType="alertType"
+       :show="show">
     {{ msg }}
   </div>
 </template>
@@ -12,18 +16,44 @@ export default {
       default: "Blank Notification",
       type: String,
       required: true
+    },
+    alertType: {
+      default: "nk-dafault",
+      type: String
+    },
+    show: {
+      default: false,
+      type: Boolean
+    }
+  },
+  computed: {
+    computedClass () {
+      let className = this.alertType
+      return className
     }
   }
-
 }
 </script>
 
 <style>
-.alert {
-  background-color: lightgray;
-  margin: 10px;
+.nk-alert {
   padding: 20px;
   font-size: 20px;
+}
+.nk-default {
+  background-color: lightgray;
   color: black;
+}
+.nk-success {
+  background-color: hsl(120, 57%, 80%);
+  color: hsl(120, 57%, 20%);
+}
+.nk-warning {
+  background-color: hsl(36, 65%, 60%);
+  color: hsl(36, 65%, 20%);
+}
+.nk-error {
+  background-color: hsl(0, 80%, 80%);
+  color: hsl(0, 80%, 20%);
 }
 </style>
